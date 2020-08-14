4e7c6069054b6cb0fbb1f077226518501d812110
constantin ostafie <costafie70@gmail.com>
2020-08-14 16:48:45 +0300

    adaugat label pe FormCalculator

--- CalculatorApp/FormCalculator.Designer.cs
+++ CalculatorApp/FormCalculator.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelformCalculator = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelformCalculator
+            // 
+            this.labelformCalculator.AutoSize = true;
+            this.labelformCalculator.Location = new System.Drawing.Point(367, 199);
+            this.labelformCalculator.Name = "labelformCalculator";
+            this.labelformCalculator.Size = new System.Drawing.Size(37, 13);
+            this.labelformCalculator.TabIndex = 0;
+            this.labelformCalculator.Text = "1+1=2";
             // 
             // FormCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelformCalculator);
             this.Name = "FormCalculator";
             this.Text = "Calculator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelformCalculator;
     }
 }
